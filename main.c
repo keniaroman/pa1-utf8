8447aa3ece54160c55fbc7af24b0fbaa0bc71cbf
keniaroman <146518158+keniaroman@users.noreply.github.com>
2024-10-08 08:10:46 +0000

    milestone #1

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ int32_t capitalize_ascii(char str[]){
     for(int i = 0; str[i] != '\0'; i++){
         if(str[i] >= 97 && str[i] <= 122){
             str[i] = str[i] - 32;
-            count += 1;
+            count++;
         }
     }
     return count;
